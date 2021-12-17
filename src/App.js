605300eec615c6528b71e8f7bcca98dc47ac7c3e
Carlos Martinez <curlosmart@gmail.com>
2021-12-17 12:21:06 -0500

    navbar finished and movies fetched from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,9 +7,21 @@ import {
   useParams
 } from "react-router-dom";
 import Navbar from './components/Navbar';
+import requests from './requests';
 import './styles.css'
 
 const App = () => {
+
+  const API_BASE_URL = `https://api.themoviedb.org/3`
+
+  useEffect(() => {
+    const fetchFromAPI = async () => {
+      const response = await axios.get(`${API_BASE_URL}${requests.fetchTrending}`)
+      console.log(response.data)
+    }
+    fetchFromAPI()
+  }, [])
+
   return (
     <div className="bg-dark text-white vw-100 vh-100">
       <Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,49 +3,59 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-transparent px-4" style={{ height: '60px'}}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-transparent px-4 fs-6" style={{ height: '80px'}}>
       <div className="container-fluid h-100">
-        <Link to="/" className="h-100" style={{ 'margin-right': '10px'}}>
-          <img src="/assets/netflix_logo.png" alt="" className="h-100"/>
+        <Link to="/" className="h-100 d-flex align-items-center" style={{ 'margin-right': '10px'}}>
+          <img src="/assets/netflix_logo.png" alt="" className="h-50"/>
         </Link>
         <div className="collapse navbar-collapse h-100" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 h-100 list-unstyled">
-            <li className="nav-item">
+            <li className="nav-item d-flex align-items-center">
               <Link to='' className="nav-link active text-white" aria-current="page" href="#">Home</Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item d-flex align-items-center">
               <Link to='' className="nav-link active text-white" aria-current="page" href="#">TV Shows</Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item d-flex align-items-center">
               <Link to='' className="nav-link active text-white" aria-current="page" href="#">Movies</Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item d-flex align-items-center">
               <Link to='' className="nav-link active text-white" aria-current="page" href="#">New & Popular</Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item d-flex align-items-center">
               <Link to='' className="nav-link active text-white" aria-current="page" href="#">My List</Link>
             </li>
           </ul>
 
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="bi-search bg-transparent border-0 text-white"></button>
-          </form>
+          <div className="d-flex align-items-center h-100">
+            <form className="d-flex px-2">
+              <input className="form-control me-2 bg-transparent text-white" type="search" placeholder="Search" aria-label="Search" />
+              <button class="bi-search bg-transparent border-0 text-white"></button>
+            </form>
 
-          <div className="nav-item dropdown h-100">
-            <div class="btn-group h-100">
-              <button type="button" class="btn btn-transparent text-white d-flex align-items-center dropdown-toggle h-100" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="/assets/netflix_avatar.png" alt="" className="h-100 rounded"/>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-end">
-                <li><button class="dropdown-item" type="button">Action</button></li>
-                <li><button class="dropdown-item" type="button">Another action</button></li>
-                <li><button class="dropdown-item" type="button">Something else here</button></li>
-              </ul>
+            <div className="nav-item px-2">
+              Kids
+            </div>
+
+            <div className="nav-item px-2">
+              DVD
+            </div>
+
+            <div className="nav-item dropdown h-100">
+              <div class="btn-group h-100">
+                <button type="button" class="btn btn-transparent text-white d-flex align-items-center dropdown-toggle h-100" data-bs-toggle="dropdown" aria-expanded="false">
+                  <img src="/assets/netflix_avatar.png" alt="" className="h-75 rounded mx-1"/>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li><button class="dropdown-item" type="button">Action</button></li>
+                  <li><button class="dropdown-item" type="button">Another action</button></li>
+                  <li><button class="dropdown-item" type="button">Something else here</button></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
