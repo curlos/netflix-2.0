bc43a40adf655cb68701915749f8e1ef86b9937b
Carlos Martinez <curlosmart@gmail.com>
2025-09-05 17:23:25 -0400

    TVEpisode.js - Replace all Axios calls with RTK Query.

--- src/pages/TVEpisode.js
+++ src/pages/TVEpisode.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {
   useParams
 } from "react-router-dom";
-import axios from 'axios';
 import TopNavbar from '../components/TopNavbar';
 import TVEpisodeBanner from '../components/TVEpisodeBanner';
 import { Spinner } from 'react-bootstrap';
-
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+import { useGetTVShowEpisodeQuery, useGetTVShowDetailsQuery } from '../services/tvApi';
 
 /**
  * @description - 
@@ -16,47 +14,23 @@ const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const TVEpisode = () => {
 
   const { id, seasonNum, episodeNum } = useParams();
-  const [episode, setEpisode] = useState();
-  const [tvShow, setTvShow] = useState();
-  const [_images, setImages] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const getAllDetails = async () => {
-      setEpisode(await getEpisodeDetails());
-      setTvShow(await getTVShow());
-      setImages(await getImages());
-      setLoading(false);
-    };
-    getAllDetails();
-  }, [episodeNum, id, seasonNum]);
-
-  const getEpisodeDetails = async () => {
-    const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}/season/${seasonNum}/episode/${episodeNum}?api_key=${API_KEY}&language=en-US`);
-    return response.data;
-  };
-
-  const getTVShow = async () => {
-    const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`);
-    return response.data;
-  };
-
-  const getVideos = async () => {
-    const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}/season/${seasonNum}/episode/${episodeNum}/videos?api_key=${API_KEY}`);
-    return response.data;
-  };
-
-  const getImages = async () => {
-    const response = axios.get(`https://api.themoviedb.org/3/tv/${id}/season/${seasonNum}/episode/${episodeNum}/images?api_key=${API_KEY}`);
-
-    return response.data;
-  };
+  
+  // RTK Query hooks
+  const { data: episode, isLoading: episodeLoading } = useGetTVShowEpisodeQuery({
+    tvId: id,
+    seasonNumber: seasonNum,
+    episodeNumber: episodeNum
+  });
+  
+  const { data: tvShow, isLoading: tvShowLoading } = useGetTVShowDetailsQuery(id);
+  
+  
+  const loading = episodeLoading || tvShowLoading;
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [id]);
 
-  getVideos();
 
   return (
     loading ? <div className="spinnerContainer"><Spinner animation="border" variant="danger" /></div> : (
